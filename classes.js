00fe2facab4b653534e2a8e59691492458c1218f
shsaci <008@saganbrainz.com>
2016-06-17 15:31:32 +1200

    make div 2 green

--- classes.js
+++ classes.js
@@ -1,24 +1,32 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+  one();
+  two();
+
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
 function one () {
   // First, we have to find the element:
-  var one = document.getElementById('one')
+  var one = document.getElementById('one');
 
   // Next, we apply a new CSS class to it:
-  one.classList.add('blue')
+  one.classList.add('blue');
 }
 
 // CREATE FUNCTION two HERE
+function two() {
+// find element 'two':
+var two = document.getElementById('two');
+
+// apply a new class to element two:
+two.classList.add('green');
+}
 
 // CREATE FUNCTION three HERE
 
